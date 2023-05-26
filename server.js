845839484300ee5463eb28ef88991645fd39a362
jpettram <131363231+jpettram@users.noreply.github.com>
2023-05-26 13:01:49 +0300

    Update server.js
    server.js muutettu expressiksi

--- server.js
+++ server.js
@@ -1,20 +1,12 @@
-const http = require('http');
-const fs = require('fs');
-const path = require('path');
+const express = require('express');
+const app = express();
 
+// Serve static files from the 'public' directory
+app.use(express.static('public'));
+
+// Handle other routes and requests
+// ...
+
+// Start the server
 const PORT = process.env.PORT || 9000;
-
-http.createServer((req, res) => {
-  if (req.url === '/') {
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    fs.createReadStream('index.html').pipe(res);
-  } else if (req.url.endsWith('.js')) {
-    const filePath = path.join(__dirname, req.url);
-    res.writeHead(200, { 'Content-Type': 'application/javascript' });
-    fs.createReadStream(filePath).pipe(res);
-  } else {
-    res.writeHead(404, { 'Content-Type': 'text/plain' });
-    res.end('404 Not Found');
-  }
-  console.log('New connection');
-}).listen(PORT, () => console.log('Listening on port', PORT));
+app.listen(PORT, () => console.log('Listening on port ', PORT));
