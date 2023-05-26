f4972db75ae49bfda4f061d154b84d65d37fb09d
jpettram <131363231+jpettram@users.noreply.github.com>
2023-05-26 13:23:45 +0300

    Update server.js

--- server.js
+++ server.js
@@ -8,5 +8,5 @@ app.use(express.static('public'));
 // ...
 
 // Start the server
-const PORT = process.env.PORT || 9000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log('Listening on port ', PORT));
