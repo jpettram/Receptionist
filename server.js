ba88fbac3b972b79dca303100c597a52807017ed
jpettram <131363231+jpettram@users.noreply.github.com>
2023-05-26 08:43:01 +0300

    Update server.js

--- server.js
+++ server.js
@@ -8,6 +8,6 @@ http
     res.writeHead(200, { 'content-type': 'text/html' })
     fs.createReadStream('index.html').pipe(res)
     // console.log('New connection')
-    //res.end('Serveri')
+    res.end('Serveri')
   })
   .listen(PORT, () => console.log('Listening on port ', PORT))
